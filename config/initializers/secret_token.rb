853eba282510d8b86bfa86e4c23b43a878e2200e
Chris Hein <heinchris@gmail.com>
2011-12-13 21:41:03 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CompassionUmc::Application.config.secret_token = 'c86b6590466d69ab332c41f04a552c575326c300625340765b6884a9e2562ecc05ad05115e52bc5b8fa3d178b1972c0b18f72680125c04ee3cdf3041d7cf17ce'
